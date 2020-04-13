7130f041a02d56af47ffc098fc86461eca122797
Chirag Bansal <chirag.bansal@mecuris.com>
2020-04-13 16:39:55 +0200

    create menu view with pagination

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -5,17 +5,55 @@ const menuItems = require('../../../assets/menu.json');
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
 
   appMenu: any;
   @Output() addToCartEvent: EventEmitter;
+  numberOfPages;
+  currentPage = 0;
+  previousButton: boolean;
+  nextButton: boolean;
+  pageChangeEmitter: EventEmitter;
 
-constructor() { }
+  constructor() { }
 
-ngOnInit() {
-    this.appMenu = menuItems;
+  ngOnInit() {
+    this.numberOfPages = Math.ceil(menuItems.length / 8);
+    this.numberOfPages = Array(this.numberOfPages);
+    this.nextButton = true;
+    this.previousButton = false;
+    this.appMenu = menuItems.slice(this.currentPage, this.currentPage * 8 + 8)
+
+  }
+
+  goTo(pageNumber) {
+    console.log(pageNumber)
+    this.currentPage = pageNumber;
+    if (pageNumber + 1 !== this.numberOfPages.length) {
+      if (pageNumber !== 0) {
+        this.nextButton = true;
+        this.previousButton = true;
+      } else {
+        this.nextButton = true;
+        this.previousButton = false;
+      }
+    } else {
+      this.nextButton = false;
+      this.previousButton = true;
+    }
+    this.appMenu = menuItems.slice(this.currentPage * 8, this.currentPage * 8 + 8)
+  }
+
+  goToNext() {
+    this.currentPage = this.currentPage + 1;
+    this.goTo(this.currentPage)
+  }
+
+  goToPrevious() {
+    this.currentPage = this.currentPage - 1;
+    this.goTo(this.currentPage)
   }
 
 }
