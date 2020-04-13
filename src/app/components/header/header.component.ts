1ce89542a0dca2296649463c187da3dae6562516
Chirag Bansal <chirag.bansal@mecuris.com>
2020-04-13 16:37:37 +0200

    create header component for navigation and cart summary

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+
+  cartItems;
+  cartItemsCount: number;
+  cartTotal: any;
+
+  constructor(private cartService: CartService, private location: Location, private router: Router) { }
 
   ngOnInit() {
+
+    this.cartService.emitCartData.subscribe((cartData) => {
+      this.cartItems = cartData;
+      this.updateCartInfo(cartData);
+    });
+  }
+
+  updateCartInfo(cartData) {
+    this.cartItemsCount = cartData.length;
+    this.cartTotal = this.cartService.cartTotal.toFixed(2).toString().replace('.', ',');
+  }
+
+  navigateToCart() {
+    this.router.navigateByUrl('/cart', { skipLocationChange: true });
+    this.location.replaceState('/cart');
+  }
+
+  navigateToMenu() {
+    this.router.navigateByUrl('menu', { skipLocationChange: true });
+    this.location.replaceState('menu');
+  }
+
+  navigateToHome() {
+    this.router.navigateByUrl('', { skipLocationChange: true });
+    this.location.replaceState('');
   }
 
 }
