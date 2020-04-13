8deb1d2ea28980216d8b5912d0b2f8fa0739c725
Chirag Bansal <chirag.bansal@mecuris.com>
2020-04-13 16:41:35 +0200

    move all components together inside a folder named components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,10 +5,11 @@ import { ItemViewComponent } from './item-view/item-view.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { CartViewComponent } from './cart-view/cart-view.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
     imports: [BrowserModule, MatSelectModule],
     exports: [ItemViewComponent, MenuComponent, HeaderComponent, FooterComponent],
-    declarations: [ItemViewComponent, MenuComponent, HeaderComponent, FooterComponent]
+    declarations: [ItemViewComponent, MenuComponent, HeaderComponent, FooterComponent, CartViewComponent, HomeComponent]
 })
 export class ComponentsModule { }
